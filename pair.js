aebdd773aae7840a44d4d3966b4c6691d343577d
kalanabby <kalanachathuranga406@gmail.com>
2025-07-29 21:12:10 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,11 +81,11 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*KC [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*https://wa.me/qr/JRZOOQIAPGCMO1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/K1gAqtv0T9m6TbWmGaPjY7*`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/kalanabby/kalanabby/5177b27aff8fdd987b4a7a06edf6a4409d438b24/ChatGPT%20Image%20Jul%2029%2C%202025%2C%2009_04_34%20PM.png",
               },
               caption: sid,
             });
